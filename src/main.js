4a0863fb40552f1fd1181c02027b5c363a012990
MatteoCantafio25 <mattyteo004@gmail.com>
2024-02-05 17:02:17 +0100

    added font awesome

--- src/main.js
+++ src/main.js
@@ -4,4 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faEnvelope);
+
+const app = createApp(App);
+
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+
+app.mount('#app');
